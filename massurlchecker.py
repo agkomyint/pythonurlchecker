05a8418dbbde0bae7d7ed1e032b89b26c759a3ba
kiskyagkomyint <51739766+agkomyint@users.noreply.github.com>
2021-02-23 17:08:36 +0630

    Add files via upload

---
+++ massurlchecker.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+#make a url text file here and add here:3
+with open("urls.txt","r") as file:
+    x=" ".join([content.strip() for content in file])
+    y=x.split()
+    print(y)
+
+#for windows user you should change ping to ping.exe incase if its not work.in subprocess.Popen inside.
+with open(os.devnull, "wb") as limbo:
+        for host in y:
+                result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", host],
+                        stdout=limbo, stderr=limbo).wait()
+                if result:
+                        print (host, " is inactive")
+                else:
+                        print (host, "is active")
